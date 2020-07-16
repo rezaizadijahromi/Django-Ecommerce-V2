de784e03100244c9023a7660a650a73417f7bfa3
rezaizadijahromi <rezaizadij2000@gmail.com>
2020-07-16 11:09:02 +0430

    Fix model manager

--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 
+from .models import Cart
+
 def cart_home(request):
-    cart_id = request.session.get('cart_id', None)
-    if cart_id is not None:
-        print("new cart created")
-        request.session["cart_id"] = 12
-    else:
-        print("Cart id is exists.")
+    cart_id = Cart.objects.get_or_new(request)
     return render(request, "carts/home.html", {})
